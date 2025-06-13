77ce0da079cea9f3496ae09e0f51186f6eb00863
RyoMagoshi <ryo_magoshi@demia.co.jp>
2025-06-14 03:53:00 +0900

    added prepare_dataset

---
+++ preprocess/prepare_dataset.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from datasets import load_dataset
+from datasets.dataset_dict import IterableDatasetDict
+from tqdm.auto import tqdm
+
+from constants.paths import INPUT_DIR
+
+
+def prepare_dataset() -> None:
+    ds = load_dataset("taln-ls2n/kp20k", trust_remote_code=True, streaming=True)
+
+    assert isinstance(ds, IterableDatasetDict)
+
+    train_data_dict = {"id": [], "text": []}
+    valid_data_dict = {"id": [], "text": []}
+
+    train_data_count = 530809
+    valid_data_count = 20000
+
+    train_char_set = set()
+
+    for data in tqdm(ds["train"], total=train_data_count):
+        train_data_dict["id"].append(data["id"])
+        train_data_dict["text"].append(data["abstract"])
+        train_char_set |= set(list(data["abstract"]))
+
+    for data in tqdm(ds["validation"], total=valid_data_count):
+        valid_data_dict["id"].append(data["id"])
+        valid_data_dict["text"].append(data["abstract"])
+
+    pd.DataFrame(train_data_dict).to_csv(INPUT_DIR / "train.tsv", sep="\t", index=False)
+    pd.DataFrame(valid_data_dict).to_csv(INPUT_DIR / "valid.tsv", sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    prepare_dataset()
